7ffc4eadf9ad6151ae2ea3c180cb6178e29bf6ee
bbortt <timon.borter@gmx.ch>
2018-10-19 16:13:05 +0200

    cleanup project according to input-output.

--- base32enc.asm
+++ base32enc.asm
@@ -1,72 +1,61 @@
 ;  Executable name : base32enc
-;  Version         : 1.0
-;  Created date    : 10/19/2018
-;  Last update     : 10/19/2018
-;  Author          : Timon Borter
-;  Description     : Program converting binary to base32 encoded string.
-;
-;  Run it this way:
-;    base32enc
-;    -> Then provide binary data via system input.
+;  Description     : Program to encode binary input to base32
 ;
 ;  Build using these commands:
-;    nasm -f elf64 -g -F dwarf base32enc.asm
+;    nasm -f elf64 -g base32enc.asm
 ;    ld -o base32enc base32enc.o
 ;
-SECTION .bss			; Section containing uninitialized data
-
-	Buffer:	resb 16
-	BufferLength: equ $-Buffer
 
 SECTION .data			; Section containing initialised data
 
-	HexStr:	db " 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",10
-	HEXLEN equ $-HexStr
+	encodingTable: db "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567", 32
 
-	Characters:	db "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
-		
+SECTION .bss			; Section containing uninitialized data
+
+	input: resb 16
+	inputLength: equ 16
+
 SECTION .text			; Section containing code
 
 global 	_start			; Linker needs this to find the entry point!
-	
+
 _start:
-	nop			; This no-op keeps gdb happy...
+	nop			; Start of program
 
-; Read a buffer full of text from stdin:
-Read:
-	mov eax,0		; Specify sys_read call
-	mov ebx,0		; Specify File Descriptor 0: Standard Input
-	mov ecx,Buffer		; Pass offset of the buffer to read to
-	mov edx,BufferLength	; Pass number of bytes to read at one pass
-	syscall			; Call sys_read to fill the buffer
-	mov ebp,eax		; Save # of bytes read from file for later
-	cmp eax,0		; If eax=0, sys_read reached EOF on stdin
-	je Done			; Jump If Equal (to 0, from compare)
+readInput:
+	mov rax, 0		; Code for sys-read call
+	mov rdi, 0		; File-Descriptor 1: Standard input
+	mov rsi, input		; Specify input location
+	mov rdx, inputLength	; Specify input size to read/write
+	syscall			; Execute read with kernel call
 
-; Set up the registers for the process buffer step:
-	mov esi,Buffer		; Place address of file buffer into esi
-	mov edi,HexStr		; Place address of line string into edi
-	xor ecx,ecx		; Clear line string pointer to 0
+	mov ebx, input	  	; Move message to register
+	cmp eax, 0		; Control if input is EOF (0 bytes) flagged
+	je exitProgramm		; Proceed to exit if ctrl+d pressed
 
-; Go through the buffer and convert binary values to base32 digits:
-Scan:
-	xor eax,eax		; Clear eax to 0
+prepareConversion:
+	mov ecx, inputLength
+	
+toBase32:
+; TODO
 
-; Bump the buffer pointer to the next character and see if we're done:
-	inc ecx		; Increment line string pointer
-	cmp ecx,ebp	; Compare to the number of characters in the buffer
-	jna Scan	; Loop back if ecx is <= number of chars in buffer
+proceedToNextChar:
+	inc ebx			; Proceed to next char
+	dec ecx			; Decreese amount of chars to process
+	jnz toBase32		; Transform next char if not finished
 
-; Write the line of encoded values to stdout:
-	mov eax,1		; Specify sys_write call
-	mov ebx,1		; Specify File Descriptor 1: Standard output
-	mov ecx,HexStr		; Pass offset of line string
-	mov edx,HEXLEN		; Pass size of the line string
-	syscall 		; Make kernel call to display line string
-	jmp Read		; Loop back and load file buffer again
+writeEncodedString:
+	mov rax, 1		; Code for sys-write call
+	mov rdi, 1		; File-Descriptor 1: Standard outp
+	mov rsi, input		; Specify input location
+	mov rdx, 16		; Specify input size to read/write
+	syscall			; Execute write with kernel kall
 
-; All done! Let's end this party:
-Done:
-	mov eax,60		; Code for Exit Syscall
-	mov ebx,0		; Return a code of zero	
-	syscall 		; Make kernel call
+	jmp readInput		; Loop until ctrl+d is pressed
+
+exitProgramm:
+	mov rax, 60		; Code for exit
+	mov rdi, 0		; Return code 0
+	syscall			; Execute exit with kernel call
+	
+	nop			; End of program
