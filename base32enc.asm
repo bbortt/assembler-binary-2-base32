12e2bae9cb0d77b11bad9708bc9d81162b0255be
bbortt <timon.borter@gmx.ch>
2018-10-31 12:17:44 +0100

    update comments.

--- base32enc.asm
+++ base32enc.asm
@@ -32,85 +32,83 @@ readInput:
 	mov rdx, inputLength	; Specify input size to read
 	syscall			; Execute read with kernel call
 
-	cmp rax, 0		; Control if input is EOF (0 bytes) flagged
-	je exitProgramm		; Proceed to exit if ctrl+d pressed
+checkShouldExitProgram:
+
+	cmp rax, 0		; Compare input size to 0 (equals ctrl+d)
+	je exitProgramm		; Proceed to exit if command received
 
 prepareRegisters:
 
-	mov r10, rax		; Move input size to r10 to detect end of encoding
+	mov r10, rax		; Persist input size as rax is used for calculations
 
 	; At this point, the following registers are in use:
 	xor eax, eax		; eax - contains parameters for the modulo calculation
 	xor ebx, ebx		; bh - contains leftovers;		bl contains shift-bits
-	xor ecx, ecx		; ecx - contains leftover-count, ecx required because of calculations
+	xor ecx, ecx		; ecx - required for calculations	cl contains leftover-count
 	xor edx, edx		; edx - contains modulo calculation results
 	xor r8, r8		; r8 - contains bytes-allocated-count
 	xor r9, r9		; r9 - contains turns-done-count
-	;			  r10 - contains input count to detect end of encoding
+	;			  r10 - contains effective input size to detect end of encoding
 	xor r15d, r15d		; r15d - contains interim results
 
 initializeData:
 
 	mov bh, [rsi]		; Read first byte as "leftovers" of the (unexisting) previous calculation
-	mov ecx, 8		; There were 8 bits left in the (unexisting) previous calculation
-	mov r8, 1		; One-time one byte was allocated (processed)
+	mov ecx, 8		; Initialize 8 leftovers, this will shift the leftovers (bh) into the shift-bits (bl)
+	mov r8, 1		; Initial byte was allocated (read from input)
 
 toBase32:
+removeAlreadyProcessedShiftBits:
 
-	inc r9			; Start new turn, increase counter
-
-shiftLeftRightRemovePrefixingLeftoverBits:
-
-	mov r15d, ecx		; Save leftover-count as interim result
+	mov r15d, ecx		; Save leftover-count as interim result because we do a shift-left-right to nullify previously processed bits
 	mov ecx, 8		; Allocate 8 to ecx to subtract leftover-count
-	sub ecx, r15d		; Subtract leftover-count from 8 to get to-nullify-bit-count
-	shl bx, cl		; Nullify bits prefixing the leftovers
+	sub ecx, r15d		; Subtract leftover-count from 8 to get the amount of already processed bits int this leftovers
+	shl bx, cl		; Nullify already processed leftovers
 	shr bx, cl		; Reset leftovers to original position
 	mov ecx, r15d		; Reallocate leftover-count to intended register
 
 shiftToFiveBase32Bits:
 
-	add ecx, 3		; Increase leftover-count by 3 bits to get 5 out of 8
+	add ecx, 3		; Increase leftover-count by 3 to get 5 out of 8 bits
 	shr bx, cl		; Shift bh+bl (=bx) to have 5 bits left
 	mov bl, [BASE32_TABLE+ebx] ; Replace encoding table index with effective base32 char
 
 addToOutput:
 
-	dec r9			; Remove one from turn-done-count to get array index (starting at 0)
 	mov [output+r9], bl	; Write current encoded char to output
-	inc r9			; Increase r9 back to turn-done-count
+	inc r9			; Increase turns-done-count, one more output processed
 
 checkShouldAllocate:
 
 	mov eax, 5		; Prepare 5 bits for every turn we did
 	mul r9			; Multiply by turns-done-count to get amount of processed bits
-	mov r15d, eax		; Save result for modulo
+	mov r15d, eax		; Save interim result for modulo calculation
 
 	mov eax, 8 		; Prepare 8 bits for every allocated byte
 	mul r8			; Multiply with bytes-allocated-count to get amount of bits already read from input
 
-	div r15d		; dx will be 8 * bytes-allocated-count % 5 * turns-done-count
-	mov cl, dl		; Copy leftover-count (modulo-result) to register
+	div r15d		; dx will be 8 * bytes-allocated-count % 5 * turns-done-count, equals leftovers to process
+	mov cl, dl		; Copy leftover-count (modulo-result) to intended register
 
 checkEndOfInputReached:
 
-	cmp cl, 8		; Check if leftovers were not nullified (e.g. end of input)
+	cmp cl, 8		; Check if leftovers were not nullified (end of input); Take a look at checkShouldAllocateZeros:
 	jge finalizeBase32String ; Jump to finalization if end of input reached
 
-	cmp cl, 0		; Check if there are no leftovers
-	jne checkShouldProcessLeftovers ; Continue encoding input
-	cmp r8, r10		; Check if we reached end of input without leftovers
+	cmp cl, 0		; Check if there are any leftovers
+	jne checkShouldProcessLeftovers ; Continue encoding input if any leftovers exist
+	cmp r8, r10		; Check if end of input without was reached without any leftovers
 	je finalizeBase32String	; Jumpt to suffixing if all input processed without leftovers
 
 checkShouldProcessLeftovers:
 
-	cmp ecx, 0		; Look if we do not have any leftovers
-	jg checkShouldAllocateFromInput	; Allocate from next byte if any leftovers exist
+	cmp ecx, 0		; Look if any leftovers exist
+	jg checkShouldAllocateFromInput	; Check if we need to allocate the next input byte if leftovers exist
 
-	inc rsi 		; Proceed to next byte from input
+	inc rsi 		; Proceed to next byte from input if no leftovers exist
 	mov bh, [rsi]		; Allocate next byte as leftovers
 	inc r8			; Increase bytes-allocated-count by 1
-	mov ecx, 8		; 0 remaining equals 8, need to shift ALL on next turn
+	mov ecx, 8		; 0 remaining equals 8, need to shift ALL leftovers to shift-bits on next turn
 	jmp toBase32		; Start algorithm from the beginning
 
 checkShouldAllocateFromInput:
@@ -122,7 +120,7 @@ checkShouldAllocateFromInput:
 
 checkShouldAllocateZeros:
 
-	cmp r8, r10		; Compare turn-done-count to input size
+	cmp r8, r10		; Compare bytes-allocated-count to input size
 	jl allocateFromInput	; Allocate from remaining input if there is some
 
 	xor bl, bl		; Nullify allocated byte if only the leftovers need to be processed
@@ -131,7 +129,7 @@ checkShouldAllocateZeros:
 allocateFromInput:
 
 	inc rsi 		; Proceed to next byte from input
-	mov bl, [rsi]		; Move input to shift-bits
+	mov bl, [rsi]		; Move input to shift-bits as we need some of them concatenated with the leftovers
 	inc r8			; Increase bytes-allocated-count by 1
 	jmp toBase32		; Start algorithm from the beginning
 
@@ -142,8 +140,8 @@ finalizeBase32String:
 
 checkShouldSuffixEncodedString:
 
-	mov eax, r9d		; Allocate turn-done-count (equal to bytes processed) to eax for modulo calculation
-	div r15d		; dx will be turn-done-count % 8
+	mov eax, r9d		; Allocate turns-done-count (equal to bytes processed) to eax for modulo calculation
+	div r15d		; dx will be turns-done-count % 8
 	cmp edx, 0		; Compare modulo result to 0 to detect multiple of 0
 	je addLineBreak		; Add final line break without suffixing if already a multiple of 8
 
@@ -151,17 +149,17 @@ suffixUntilMultipleOf8:
 
 	mov [output+r9], byte '=' ; Write suffix ('=') to fill up to multiple of 8
 
-	mov eax, r9d		; Allocate turn-done-count (equal to bytes processed) to eax for modulo calculation
-	div r15d		; dx will be turn-done-count % 8
+	mov eax, r9d		; Allocate turns-done-count (equal to bytes processed) to eax for modulo calculation
+	div r15d		; dx will be turns-done-count % 8
 	cmp edx, 0		; Compare modulo result to 0 to detect multiple of 0
 	je addLineBreak		; Add final line break if a multiple of 8
 
-	inc r9			; Increase turn-done-count by one
+	inc r9			; Increase turns-done-count by one
 	jmp suffixUntilMultipleOf8 ; Loop suffixing until we reach multiple of 8
 
 addLineBreak:
 
-	mov [output+r9], byte 10 ; Add line-break ad the end of output
+	mov [output+r9], byte 10 ; Add line-break to the end of output
 
 writeEncodedString:
 
